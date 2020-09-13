83c63c95fb32f1a7bfdf14ac067cc379a4b8548e
易成杰 <626659321@qq.com>
2020-09-13 22:16:12 +0800

    init

---
+++ oauth-github-study/src/main/java/com/yicj/security/controller/SimpleController.java
@@ -0,0 +1,15 @@
+package com.yicj.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String hello(Principal principal){
+        return "hello, " + principal.getName();
+    }
+}
